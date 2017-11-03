d707001edd542e9d9f9bd0f5149569352bb9ab37
Marcos Felipe Eipper <marcos@eipper.com.br>
2017-11-03 14:43:20 -0200

    Desempenho melhorado.

--- genetic1.py
+++ genetic1.py
@@ -1,20 +1,18 @@
 
 import string
 import random
-from numpy.random import choice
+from random import choice
+#from numpy.random import choice
 import numpy as np
 from numpy import cumsum
 from numpy.random import rand
-def weightedChoice(weights, objects):
-    """Return a random item from objects, with the weighting defined by weights
-    (which must sum to 1)."""
-    cs = cumsum(weights) #An array of the weights, cumulatively summed.
-    idx = sum(cs < rand()) #Find the index of the first weight over a random value.
-    return objects[idx]
+import os
+from numba import jit
 
+lines = os.get_terminal_size().lines
 chars = string.ascii_letters + ',' + '.' + ' '
 
-class Individual():
+class Individual:
     def __init__(self, lenght, dna=[], objective=""):
         self.lenght = lenght
         self.fitness = 0
@@ -25,17 +23,27 @@ class Individual():
             self.dna = dna
 
     def crossover(self, other):
+        new_dna = []
+        # for i in range(len(self.dna)):
+        #     if i % 2 == 0:
+        #         new_dna.append(self.dna[i])
+        #     else:
+        #         new_dna.append(other.dna[i])
+
         half = len(self.dna) // 2
         new_dna = self.dna[0:half] + other.dna[half:]
         res = Individual(self.lenght, dna=new_dna, objective=self.objective)
+        res.calc_fitness()
         res.mutate()
         return res
 
     def mutate(self):
-        if self.fitness < random.uniform(0.2, 1.0):
-            n = random.randint(0, (self.lenght // 2)-1)
+        if self.fitness > random.uniform(0.0, 0.9):
+        #if 0.5 <= random.uniform(0.0, 1.0):
+            n = random.randint(0, (self.lenght // 2))
             for i in range(n):
-                self.dna[random.randint(0, self.lenght-1)] = random.choice(chars)
+                self.dna[random.randint(0, self.lenght-1)] = choice(chars)
+            self.calc_fitness()
 
     def show(self):
         res = ""
@@ -64,48 +72,62 @@ class Individual():
 
 
 
-obj = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse molestie ut nisi et condimentum. Proin pharetra, neque ut eleifend dignissim, sapien est aliquet lorem, at ullamcorper quam libero eget orci. Nunc dapibus dui quis nisi sodales, ac pharetra neque porttitor. Pellentesque rutrum vestibulum volutpat. Quisque tincidunt auctor dolor, eu ultricies mi suscipit quis. Nam dolor metus, facilisis nec lorem aliquam, lacinia sollicitudin neque. Morbi at egestas eros. Nunc molestie, turpis id iaculis congue, diam elit commodo tellus, a eleifend risus nibh eu lacus. Nunc pellentesque dui et quam aliquet, eget auctor ipsum porttitor."
-obj = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-num_individuals = 100
-modifier = 0.1 # Selection easening
+#obj = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse molestie ut nisi et condimentum. Proin pharetra, neque ut eleifend dignissim, sapien est aliquet lorem, at ullamcorper quam libero eget orci. Nunc dapibus dui quis nisi sodales, ac pharetra neque porttitor. Pellentesque rutrum vestibulum volutpat. Quisque tincidunt auctor dolor, eu ultricies mi suscipit quis. Nam dolor metus, facilisis nec lorem aliquam, lacinia sollicitudin neque. Morbi at egestas eros."
+obj = "Unicorns are fun. Dinosaurs too."
+#obj = "hello, worldee"
+num_individuals = 2048
+modifier = 0.01 # Selection easening
+gen = 1
+elitism = 0.1
+
+def tournament(population):
+    best = choice(population)
+    for i in range(3):
+        aux = choice(population)
+        if (aux.fitness > best.fitness): best = aux
+    return best
 
 pop = [Individual(lenght=len(obj), objective=obj) for n in range(num_individuals)]
 print(pop[1].dna)
 print(pop[1].objective)
 current_fitness = 0
 found = False
+for ind in pop:
+    ind.calc_fitness()
+
+idx = round(elitism * num_individuals)
+
 while 1:
+
+
+    #pop_best = list(filter(lambda x : x.fitness > current_fitness-modifier, pop))
+    best_individuals = sorted(pop, key = lambda x : x.fitness, reverse=True)
+    fitness_list = [ind.fitness for ind in best_individuals]
+    current_fitness = np.average(fitness_list)
+
+    print("Gen " + str(gen) + "\tavg. fitness: " + str(current_fitness)
+         + "\tElitism: " + str(elitism))
+    print()
+
+    new_gen = []
+
+    while len(new_gen) != num_individuals:
+        ind1 = tournament(best_individuals[:idx])
+        ind2 = tournament(best_individuals[:idx])
+        if random.uniform(0, 1) <= 0.8:
+            new_gen.append(ind1.crossover(ind2))
+            new_gen.append(ind2.crossover(ind1))
+
+    pop = new_gen
+    gen += 1
     for ind in pop:
         ind.show()
-        ind.calc_fitness()
         if ind.dna == ind.objective:
-            ind.show()
+            print(gen)
             found = True
             break
     if found:
         print("Population avg fitness:" + str(current_fitness))
         break
 
-
-    pop_best = list(filter(lambda x : x.fitness > np.random.uniform(current_fitness-modifier, current_fitness), pop))
-    best_individuals = sorted(pop_best, key = lambda x : x.fitness, reverse=True)
-    fitness_list = [ind.fitness for ind in best_individuals]
-    new_fitness = np.average(fitness_list)
-    if current_fitness < new_fitness:
-        current_fitness = new_fitness
-    #print(fitness_list)
-    print(current_fitness)
-    print(len(pop_best))
-    if len(pop_best) < 10:
-        modifier += 0.1
-    if modifier >= 0.5:
-        modifier -= 0.1
-
-
-    new_gen = []
-    for _ in range(num_individuals):
-        ind1 = weightedChoice(fitness_list, best_individuals)
-        ind2 = weightedChoice(fitness_list, best_individuals)
-        new_gen.append(ind1.crossover(ind2))
-
-    pop = new_gen
+    if gen % 100 == 0: elitism += 0.001
