ec1c36d0183dbb7ad7869ab4926a0294f292c5e3
Marcos Felipe Eipper <marcos@eipper.com.br>
2017-11-02 23:22:13 -0200

    Ajustes

--- genetic1.py
+++ genetic1.py
@@ -12,6 +12,8 @@ def weightedChoice(weights, objects):
     idx = sum(cs < rand()) #Find the index of the first weight over a random value.
     return objects[idx]
 
+chars = string.ascii_letters + ',' + '.' + ' '
+
 class Individual():
     def __init__(self, lenght, dna=[], objective=""):
         self.lenght = lenght
@@ -30,10 +32,10 @@ class Individual():
         return res
 
     def mutate(self):
-        if self.fitness < random.uniform(0.5, 1.0):
-            n = random.randint(0, self.lenght-1)
+        if self.fitness < random.uniform(0.2, 1.0):
+            n = random.randint(0, (self.lenght // 2)-1)
             for i in range(n):
-                self.dna[random.randint(0, self.lenght-1)] = random.choice(string.ascii_lowercase + " " + ", ")
+                self.dna[random.randint(0, self.lenght-1)] = random.choice(chars)
 
     def show(self):
         res = ""
@@ -57,13 +59,15 @@ class Individual():
     def generate_dna(self):
         res = []
         for char in self.objective:
-            res.append(random.choice(string.ascii_lowercase + " " + ", "))
+            res.append(random.choice(chars))
         return res
 
 
 
-obj = "hello, world"
+obj = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse molestie ut nisi et condimentum. Proin pharetra, neque ut eleifend dignissim, sapien est aliquet lorem, at ullamcorper quam libero eget orci. Nunc dapibus dui quis nisi sodales, ac pharetra neque porttitor. Pellentesque rutrum vestibulum volutpat. Quisque tincidunt auctor dolor, eu ultricies mi suscipit quis. Nam dolor metus, facilisis nec lorem aliquam, lacinia sollicitudin neque. Morbi at egestas eros. Nunc molestie, turpis id iaculis congue, diam elit commodo tellus, a eleifend risus nibh eu lacus. Nunc pellentesque dui et quam aliquet, eget auctor ipsum porttitor."
+obj = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 num_individuals = 100
+modifier = 0 # Selection easening
 
 pop = [Individual(lenght=len(obj), objective=obj) for n in range(num_individuals)]
 print(pop[1].dna)
@@ -72,7 +76,7 @@ current_fitness = 0
 found = False
 while 1:
     for ind in pop:
-        ind.show()
+        find.show()
         ind.calc_fitness()
         if ind.dna == ind.objective:
             ind.show()
@@ -83,7 +87,7 @@ while 1:
         break
 
 
-    pop_best = list(filter(lambda x : x.fitness > random.uniform(current_fitness-(0.3), current_fitness), pop))
+    pop_best = list(filter(lambda x : x.fitness > np.random.uniform(current_fitness-modifier, current_fitness), pop))
     best_individuals = sorted(pop_best, key = lambda x : x.fitness, reverse=True)
     fitness_list = [ind.fitness for ind in best_individuals]
     new_fitness = np.average(fitness_list)
@@ -92,9 +96,16 @@ while 1:
     #print(fitness_list)
     print(current_fitness)
     print(len(pop_best))
+    if len(pop_best) < 10:
+        modifier += 0.1
+        print("Mod up!")
+    if modifier >= 0.5 and len(pop_best) >= 10:
+        modifier -= 0.1
+        print("Mod too high, going down.")
+
 
     new_gen = []
-    for i in range(num_individuals):
+    for _ in range(num_individuals):
         ind1 = weightedChoice(fitness_list, best_individuals)
         ind2 = weightedChoice(fitness_list, best_individuals)
         new_gen.append(ind1.crossover(ind2))
